af073cac52729f058fd24431d99983061e6bd8d2
Rebekah <rebekah.kivett@gmail.com>
2021-05-10 15:38:41 -0500

    correct database change

--- config/Database.php
+++ config/Database.php
@@ -7,20 +7,23 @@
     
     
     // DB Params for Heroku
-    private $host = 'frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-    private $db_name = 'i22jce46fiw00cqe';
-    private $password = 'mp8kaoxektltu4de';
     private $conn;
 
     // DB Connect
     public function connect() {
+      $url = getenv('JAWSDB_URL');
+      $dbparts = parse_url($url);
+
+      $hostname = $dbparts['host'];
+      $username = $dbparts['user'];
+      $password = $dbparts['pass'];
+      $database = ltrim($dbparts['path'],'/');
+
+      $dsn = "mysql:host={$hostname};dbname={$database}";
       $this->conn = null;
 
       try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username,
-        $this->password
-        );
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->conn = new PDO($dsn, $username, $password);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
       }
