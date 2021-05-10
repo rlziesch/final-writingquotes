39df64aa39ab109a54f89c35a16639f45fe9e8ed
Rebekah <rebekah.kivett@gmail.com>
2021-05-10 15:26:38 -0500

    Change database config to Heroku

--- config/Database.php
+++ config/Database.php
@@ -1,10 +1,15 @@
 <?php 
   class Database {
-    // DB Params
-    private $host = 'localhost';
-    private $db_name = 'quotesdb';
-    private $username = 'root';
-    //private $password = '';
+    // DB Params for local
+    // private $host = 'localhost';
+    // private $db_name = 'quotesdb';
+    // private $username = 'root';
+    
+    
+    // DB Params for Heroku
+    private $host = 'frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    private $db_name = 'i22jce46fiw00cqe'
+    private $password = 'mp8kaoxektltu4de';
     private $conn;
 
     // DB Connect
@@ -13,7 +18,7 @@
 
       try { 
         $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username,
-        //$this->password
+        $this->password
         );
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
